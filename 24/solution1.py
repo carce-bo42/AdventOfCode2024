2598f57b853567e2fb208f8a2aa5bc462d037ace
Christian Arce <yerildtt@gmail.com>
2024-12-24 13:50:28 +0100

    paro que es navidad

--- 24/solution1.py
+++ 24/solution1.py
@@ -9,8 +9,8 @@ def read_file(filename: str) -> tuple[dict[str, int], list[tuple[str,str,str,str
             if m := re.match(r"^(\w+): ([01])$", line.strip()):
                 wires[m.group(1)] = int(m.group(2))
             elif m := re.match(r"^(\w+) ([A-Z]+) (\w+) -> (\w+)$", line.strip()):
-                wires.setdefault(m.group(1), -1)
-                wires.setdefault(m.group(3), -1)
+                wires.setdefault(m.group(1), None)
+                wires.setdefault(m.group(3), None)
                 gates.append((m.group(1),m.group(2),m.group(3),m.group(4)))
 
     return wires, gates
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     while len(gates) != 0:
         for gate in gates:
-            if wires[gate[0]] == -1 or wires[gate[2]] == -1:
+            if wires[gate[0]] is None or wires[gate[2]] is None:
                 continue
             else:
                 wires[gate[3]] = operators[gate[1]](wires, gate[0], gate[2])
@@ -47,7 +47,5 @@ if __name__ == "__main__":
         if wire[0] == 'z':
             zwires.append((wire,value))
     zwires.sort(reverse=True)
-    result_str = "".join([str(s[1]) for s in zwires])
-    result = int(result_str,base=2)
-    print(result)
+    print(int("".join([str(s[1]) for s in zwires]),base=2))
 
