9bfc7c2d9f7baad18e84e878255dbe69ee4bf354
Christian Arce <yerildtt@gmail.com>
2025-01-01 22:08:29 +0100

    Algo esta muy muy mal en lo que hago lol

--- 16/solution2.py
+++ 16/solution2.py
@@ -1,49 +1,57 @@
-from collections import deque
 from heapq import heapify, heappop, heappush
+from colorama import Fore, Style
+from collections import deque
+
+class _Map:
+
+    # X_LEN = 18
+    # Y_LEN = 18
+    X_LEN = 142
+    Y_LEN = 142
+
+    def __init__(self, x_len = X_LEN, y_len = Y_LEN):
+        self.x_len = x_len
+        self.y_len = y_len
+        self._map = [['.'] * x_len for _ in range(y_len)]
+
+    def __getitem__(self, indices):
+        x, y = indices
+        return self._map[y][x]
+
+    def __setitem__(self, indices, value):
+        x, y = indices
+        self._map[y][x] = value
+
+    def __repr__(self):
+        # Generate rows as strings with ">" in a different color
+        rows_as_strings = [
+            " ".join(
+                Fore.RED + str(cell) + Style.RESET_ALL if cell in "O" else str(cell)
+                for cell in row
+            )
+            for row in self._map
+        ]
+        return "\n".join(rows_as_strings)
 
 ROUTING = {
     # current direction: tuple[(dx,dy), next direction, cost)]
-    "^": ( ((0,-1), "^", 1   ), ((1,0), ">", 1001), ((-1,0), "<", 1001)),
-    ">": ( ((0,-1), "^", 1001), ((1,0), ">", 1   ), (( 0,1), "v", 1001)),
+    "^": ( ((0,-1), "^", 1   ), ((1, 0), ">", 1001), ((-1,0), "<", 1001)),
+    ">": ( ((0,-1), "^", 1001), ((1, 0), ">", 1   ), ((0,1), "v", 1001)),
     "v": ( ((1, 0), ">", 1001), ((0,1), "v", 1   ), ((-1,0), "<", 1001)),
     "<": ( ((0,-1), "^", 1001), ((0,1), "v", 1001), ((-1,0), "<", 1   ))
 }
 
 DIRECTIONS = {
-    ">" : (1,0),
-    "v" : (0,1),
-    ">" : (-1,0),
-    "^" : (0,-1)
+    (0,1),
+    (0,-1),
+    (1,0),
+    (-1,0)
 }
-
-def backwards_bfs(end: tuple[int,int], distances: dict[tuple[int,int], int]):
-    queue = deque()
-    res = 1
-    start = (*end, distances[end][0], "<")  # x, y, score, direction
-    queue.append(start)
-    visited = set()
-
-    while queue:
-        cur_x, cur_y, cur_score, cur_dir = queue.popleft()
-
-        allowed_directions_n_score = []
-        for vec, dir, score in ROUTING[cur_dir]:
-            allowed_directions_n_score.append((vec, dir, cur_score - score))
-
-        for new_vec, new_dir, new_score in allowed_directions_n_score:
-            neighbor = (cur_x + new_vec[0], cur_y + new_vec[1])
-            if neighbor in distances \
-                and (distances[neighbor][0] in [new_score, new_score - 1000]) \
-                    and neighbor not in visited:
-                res += 1
-                queue.append((*neighbor, new_score, new_dir))
-                visited.add(neighbor)
-
-    return res
 
 def dijkstra(points: set[(int,int)], source: tuple[int,int] ) -> dict[(int,int) : int]:
 
     distances = { source : (0, ">") }
+    predecessors = {source: None}
 
     pq = [(0, source)]
     heapify(pq)
@@ -62,18 +70,63 @@ def dijkstra(points: set[(int,int)], source: tuple[int,int] ) -> dict[(int,int) 
                 new_dist = current_cost + cost
                 if neighbor not in distances or new_dist < distances[neighbor][0]:
                     distances[neighbor] = (new_dist, direction)
+                    predecessors[neighbor] = (current_node, direction)
                     heappush(pq, (new_dist, neighbor))
 
-    return distances
+    return distances, predecessors
+
+
+def backtrack_values(
+        distances: dict[tuple[int,int] : int],
+        end: tuple[int,int],
+        _map
+    ) -> int:
+
+    queue = deque()
+    visited = set()
+
+    max_distance_allowed = distances[end]
+    current_min_score = 1000000000
+
+    sits = 1
+    queue.append(end)
+
+    while queue:
+        node = queue.popleft()
+        x, y = node
+        current_score = distances[node]
+
+        for dx,dy in DIRECTIONS:
+            neighbor = (x+dx,y+dy)
+            if neighbor in visited or neighbor not in distances:
+                continue
+
+            score = distances[neighbor]
+            if score > max_distance_allowed or score > current_min_score + 1001:
+                continue
+
+            score_diff = current_score - score
+            if score_diff in [1, 1001, -999]:
+                #print(score_diff, current_score)
+                sits += 1
+                visited.add(neighbor)
+                queue.append(neighbor)
+                _map[*node] = 'O'
+                if current_min_score > score:
+                    current_min_score = score
+
+    return sits
 
 
 def read_file(filename: str):
 
     points = set()
+    _map = _Map()
     with open(filename) as file:
         for y, line in enumerate(file):
             for x, char in enumerate(line):
 
+                _map[x,y] = char
                 if char == '#':
                     continue
 
@@ -85,10 +138,19 @@ def read_file(filename: str):
                 if char == 'E':
                     end = (x,y)
 
-    return points, start, end
-
+    return _map, points, start, end
 
 if __name__ == "__main__":
-    points, start, end = read_file("input.txt")
-    distances = dijkstra(points, start)
-    print(backwards_bfs(end, distances))
+    _map, points, start, end = read_file("input.txt")
+    distances, predecessors = dijkstra(points, start)
+    print(distances[end])
+    sits = backtrack_values({point : distances[point][0] for point in distances.keys()}, end, _map)
+    print(sits)
+    print(_map)
+    # for y in range(0,_map.y_len-1):
+    #     for x in range(0, _map.x_len-1):
+    #         if (x,y) in distances:
+    #             print(f"{distances[(x,y)][0]:5}", end=" ")
+    #         else:
+    #             print("    #",end=" ")
+    #     print()
